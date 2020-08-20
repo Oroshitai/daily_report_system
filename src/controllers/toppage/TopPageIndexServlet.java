18fab06a944b6f4e388e9f0c5495fa934fdf9b02
shinya.takaramoto <rm0063vpedc15@gmail.com>
2020-08-20 23:06:49 +0900

    Modify pagination

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -60,7 +60,7 @@ public class TopPageIndexServlet extends HttpServlet {
 		request.setAttribute("reports_count", reports_count);
 		request.setAttribute("page", page);
 
-		if(request.getSession().getAttribute("flash") != null){
+		if(request.getSession().getAttribute("flush") != null){
 			request.setAttribute("flush", request.getSession().getAttribute("flush"));
 			request.getSession().removeAttribute("flush");
 		}
